8e41685ebbd427acce770f45854a3b45a6a8b10e
Nisha Raichur <nisha.raichur@uni-ulm.de>
2019-12-19 09:23:47 +0100

    Modified to process the images in batches of 100

--- main.py
+++ main.py
@@ -8,57 +8,50 @@ from RecurrentNets import RNN, RecurrentCell
 
 
 #Divides the data into batches
-def train_rnn(net,data,batch,loss,train_lbs,epochs=10,lr=0.001):
-    data_length=data.shape[0]
+def train_rnn(net, data, loss, batch, epochs, lr,train_lbs):
+    data_length = data.shape[0]
     for e in range(epochs):
+        print("epoch ", e)
         avg_loss = 0.0
         lossval = 0.0
-        index=0
-        while(index!=data_length):
-            lossval=rnn_step(net,data,index,batch,loss,lr,train_lbs=train_lbs)
-            avg_loss+=lossval/data_length
-            index=index+batch
-        print("Epoch {} of {}; loss={}.".format(e,epochs,avg_loss),end="\r") 
+        regularizer = tf.constant(0.0005)
+        decayRate = 0.1
+        decayStep = 40
+        indices = tf.random.shuffle(tf.range(data_length - batch -1))
+        v = tuple(tf.zeros(shape = x.shape) for x in net.variables)
+        for idx in indices:
+            trainData = data[idx:idx+batch,:,:,:]
+            labels = tf.one_hot(train_lbs[idx:idx+batch],10)
+            lossval, v = gradient_loss(net, trainData, labels,loss,lr,regularizer,v,decayRate,decayStep,epochs)
+            avg_loss += lossval / indices.shape[0]
+        print("Epoch {} of {} : loss = {}.".format(e, epochs, avg_loss), end='/r')
 
-
-def cross_loss(predict, a, variables, regularizer):
-    error = 0
-    norm = 0
-    for keys in predict:
-        error = error + tf.reduce_sum(a * tf.math.log(tf.clip_by_value(predict[keys], 1e-10, 1.0)) + (1-a) * tf.math.log(tf.clip_by_value((1 - predict[keys]), 1e-10, 1.0)))
-    
-    for x in variables:
-        norm = norm + tf.linalg.norm(x)
-    return(-error * norm) 
-
-#Iterates over mini-batch of 100
-def rnn_step(net,data,index,batch,loss,lr,train_lbs):
-    avg_loss=0
-    loss_val=0
-    for i in range(batch):
-        loss_val=gradient_loss(net,data[i+index:i+1+index,:,:],train_lbs[i],loss,lr)
-        avg_loss+=loss_val  
-    return avg_loss
 
 #Calculates the gradient and updates the model variables
-def gradient_loss(net, data, actualOutput, loss, lr, regularizer):
+def gradient_loss(net, data, actualOutput, loss, lr, regularizer, v, decayRate, decayStep, epochs):
     with tf.GradientTape() as t:
-        t.watch(net.variables)
-        prediction=net(data,4)
-        loss_val=loss(prediction, tf.one_hot(actualOutput, 10), net.variables, regularizer)
-    gradients = t.gradient(loss_val, net.variables)
-    print(len(net.variables))
-    for g, v in zip(gradients, net.variables):
-        v.assign_sub(lr * g)
-    return(loss_val)
+        predicted = net(data, 4)
+        norm = 0.0
+        loss_val = tf.reduce_mean([tf.reduce_mean(loss(actualOutput,x)) for x in predicted.values()])
+    for x in net.variables:
+        norm = norm + tf.linalg.norm(x)
+    loss_regularized = loss_val + (norm * regularizer) 
+    gradients = t.gradient(loss_regularized, net.variables)
+    learningRate = lr * pow(decayRate, (epochs / decayStep))
+    v = tuple((x * 0.9) - (learningRate * y) for x, y in zip(v, gradients))
+    for x in range(len(net.variables)):
+        net.variables[x].assign_add(v[x]) 
+    return loss_regularized, v
 
 
 def main():
-	#Intially trained a model on mnist dataset 
-	(train_imgs,train_lbs),(test_imgs,test_lbs)=tf.keras.datasets.mnist.load_data()
-	train_imgs=train_imgs.reshape(-1, 28, 28, 1)
-	net=RNN(train_imgs[0:1,:,:].shape,3)
-	train_rnn(net,data=tf.convert_to_tensor(train_imgs[0:50,:,:,:],dtype="float32"),batch=10,loss=cross_loss)
+	(train_imgs, train_lbs), (test_imgs, test_lbs) = tf.keras.datasets.mnist.load_data()
+	train_imgs = train_imgs.reshape(-1, 28, 28, 1)
+	oneImage = train_imgs[0:100, :, :, :]
+	oneImage = tf.convert_to_tensor(oneImage,dtype="float32")
+	net = RNN(oneImage.shape, 3)
+	train_rnn(net,data=tf.convert_to_tensor(train_imgs[0:500,:,:,:],dtype="float32"),loss=tf.nn.softmax_cross_entropy_with_logits,batch=100,epochs=10,lr=0.1,train_lbs=train_lbs)
+
 
 
 if __name__=='__main__':
